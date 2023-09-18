688e3f4b7c2abffcfb381b1976296e67744bfbd5
chamindu <chamindukasun074"gmail.com>
2023-09-18 12:07:56 +0530

    Getting access token and tracks of playlists combined

--- API/GetAllPlaylistItems/index.js
+++ API/GetAllPlaylistItems/index.js
@@ -1,16 +1,16 @@
-const axios = require('axios');
+const axios = require("axios");
 
-// Function to fetch tracks from a playlist by its ID
 async function fetchTracksFromPlaylist(playlistId, accessToken) {
-  //console.log('Fetching tracks from playlist');
   try {
-    const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    const response = await axios.get(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
 
-    // Process the response data and extract the required information
     const playlistDetails = {
       playlistId,
       tracks: response.data.items.map((item) => ({
@@ -25,11 +25,8 @@ async function fetchTracksFromPlaylist(playlistId, accessToken) {
     console.error(`Error fetching playlist ${playlistId}: ${error.message}`);
     return null;
   }
-
-
 }
 
-//export playlistDetails to be used in other files
 module.exports = {
-  fetchTracksFromPlaylist
- };
+  fetchTracksFromPlaylist,
+};
--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-const fetchTracks = require('./API/GetAllPlaylistItems/index.js');
-const authOptions = require('./API/TokenAccessAPI/index.js');
-const fetchToken = require('./API/TokenAccessAPI/index.js');
+const fetchTracks = require("./API/GetAllPlaylistItems/index.js");
+const authOptions = require("./API/TokenAccessAPI/index.js");
+const fetchToken = require("./API/TokenAccessAPI/index.js");
 
-// An array of playlist IDs you want to fetch
-const playlistIds = ['37i9dQZF1DX4QcmlTAbT3k'];    // 'playlist_id_1', 'playlist_id_2', 'playlist_id_3'
+const playlistIds = [
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+  "37i9dQZF1DX4QcmlTAbT3k",
+];
 
-fetchToken.fetchToken()
-  .then((accessToken) => {
-    console.log('Access Token:', accessToken);
+function splitAndConcatArray(array, size) {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    if (chunk.length > 0) {
+      result.push(chunk.join(", "));
+    }
+  }
+  return result;
+}
 
-    // Now you can use the token in the rest of your main index.js logic
-    // ...
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-
-// fetchTracks.fetchTracksFromPlaylist(accessToken, playlistIds[0]);
-
+(async () => {
+  try {
+    const tracks = [];
+    const accessToken = await fetchToken.fetchToken();
+    const concatedTracks = splitAndConcatArray(playlistIds, 16);
+    for (let i = 0; i < concatedTracks.length; i++) {
+      const results = await fetchTracks.fetchTracksFromPlaylist(
+        accessToken,
+        concatedTracks[0]
+      );
+      tracks.push(results);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
