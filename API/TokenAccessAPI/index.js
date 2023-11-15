c9c978df1f87a449571ea685b645ad1b9e7b88bb
Ranidugamage <mgyoshitharanidu@gmail.com>
2023-11-15 11:53:14 +0530

    token and playlist change

--- API/TokenAccessAPI/index.js
+++ API/TokenAccessAPI/index.js
@@ -2,8 +2,8 @@ const request = require("request");
 
 function fetchToken() {
   return new Promise((resolve, reject) => {
-    const client_id = "0468a58959134e27b4cd81bd1d3d2af5";
-    const client_secret = "6988f1ad22f5432084eaf21d4f3813cf";
+    const client_id = "c06eb2e1b5ec4b58b769ea1d3488df0b";
+    const client_secret = "e71d1b959ebc43f88c8270e3337d47b9";
 
     const authOptions = {
       url: "https://accounts.spotify.com/api/token",
